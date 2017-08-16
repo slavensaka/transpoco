c0dc38781423df2cfd95b7979c1ef4dbbbfe46ad
Slaven Sakačić <slavensakacic@gmail.com>
2017-08-16 22:03:26 +0200

    Initial commit

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Chatroom</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+ <div id="app">
+  <h1>chat</h1>
+  <example></example>
+ </div>
+
+ <script src="js/app.js" charset="utf-8"></script>﻿
+</body>
+</html>
